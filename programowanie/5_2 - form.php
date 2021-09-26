b0ec4cc92b9622f47d48afa93f5bca315bfce3a3
przybyłstanisław <90000924+stanley220@users.noreply.github.com>
2021-09-27 00:54:20 +0200

    Create 5_2 - form.php

---
+++ programowanie/5_2 - form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Formularz</title>
+  </head>
+  <body>
+    <form action="5_1_data.php" method="get">
+      <input type="text" name="name" placeholder="Podaj imię"><br><br>
+      <input type="text" name="nationality" placeholder="Podaj narodowość"><br><br>
+      <input type="number" name="age" placeholder="Podaj wiek"><br><br>
+      <input type="submit" value="Wyślij dane">
+    </form>
+  </body>
+</html>
