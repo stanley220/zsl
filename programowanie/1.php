6bf00462ec7cc7066571ac42197c91e3a74c48d9
przybyłstanisław <90000924+stanley220@users.noreply.github.com>
2021-10-04 10:12:58 +0200

    Add files via upload

--- programowanie/1.php
+++ programowanie/1.php
@@ -2,52 +2,54 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Średni wiek</title>
 </head>
 <body>
+
     <?php
-    echo "zsł<br>";
-    echo "zsł<br>";
+        if (!isset($_POST['person']) && !isset($_POST['buttonAvg'])) {
+            echo <<< FORMCOUNTPERSON
+            <h3>Ilość osób w rodzinie</h3>
+            <form method="post">
+                <input type="number" name="person" placeholder="Podaj ilość osób"><br><br>
+                <input type="submit" value="Zatwierdź">
+            </form>
+        FORMCOUNTPERSON;
+        }
+    ?>
 
-    $name="Stanisław";
-    echo "Moje imię to $name <br>";
-    echo 'Moje imię to $name <br>';
-    
-    echo "Stanislaw"." Przybył<br>";
+    <?php
+        if (!empty($_POST['person'])) {
+            echo "<h3>Ilość osób w rodzinie: $_POST[person]</h3>";
+            echo <<< FORMAGEPERSON
+                <form method="post">
+        FORMAGEPERSON;
 
-    //typy danych
-    //całkowity
-    $calkowita=10;
-    $bin=0b101;//5
-    $oct=011;//9
-    $hex=0x11;//17
-    echo $hex."<br>";
+            for ($i=1; $i <= $_POST['person']; $i++) {
+                echo "<input type=\"number\" name=\"age$i\" placeholder=\"Podaj wiek osoby $i\"><br><br>";
+            }
+            echo '<input type="submit" name="buttonAvg" value="Oblicz średni wiek">';
 
-    //zmiennoprzecinkowe
-    $v=10.5;
+            echo <<< FORMAGEPERSON
+                </form>
+        FORMAGEPERSON;
+        }
 
-    //logiczny
-    $prawda=true;
-    $fałsz=false;//w zmiennych nie dajemy polskich znaków
-    echo $prawda;
-    echo $fałsz;
-    //składnia heredox
-    echo <<< E
-        <br>Twoje imie to: $name<br>
-    E;
-
-    $text = <<< E
-        <br>tekst<br>
-    E;
-    echo $text;
-
-    echo <<< "E"
-        <br>Twoje imie to: $name<br>
-    E;
-
-    $costam="Nowak";
-    echo "\$name $costam";
+        if (isset($_POST['buttonAvg'])) {
+            //print_r($_POST);
+            $ageAvg=0;
+            foreach ($_POST as $key => $value) {
+              echo "$key: $value<br>";
+              if ($key != 'buttonAvg') {
+                $ageAvg=$ageAvg+$value;
+              }
+            }
+            echo "Średni wiek: $ageAvg";
+        }
+        //Dodać zaokrąglenie wyniku do 2 miejsc po przecinku (number_format)
+        //Dodać hiperłącze wyświetlające początkowy formularz (header('location: '))
     ?>
 </body>
 </html>
