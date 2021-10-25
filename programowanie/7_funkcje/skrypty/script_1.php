899cf3bfdea287c8d29bcf63a6a79af6d7d81473
przybyłstanisław <90000924+stanley220@users.noreply.github.com>
2021-10-25 10:44:47 +0200

    Add files via upload

--- programowanie/7_funkcje/skrypty/script_1.php
+++ programowanie/7_funkcje/skrypty/script_1.php
@@ -1,10 +1,21 @@
-<?php
-  if (!empty($_POST['name'])) {
-    require_once '../funkcje/function_1.php';
-    //echo text();
-    $string="lokal";
-    echo name('Krystyna').'<br>';
-    echo 'Imie: '.name($_POST['name']).'<br>';
-    echo validateString($_POST['name'], 10);
-  }
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h3>Dane użytkownika</h3>
+    <?php
+      require_once '../funkcje/function_1.php';
+      echo "<hr>";
+      if (!empty($_POST['name']) && !empty($_POST['color']) && !empty($_POST['nationality'])) {
+        echo "Imię bez poprawy: ".$_POST['name'].", długość: ".strlen($_POST['name'])."<br>";
+        $color=$_POST['color'];
+        echo "<div style=\"color: $color\">";
+        echo "Imię po poprawie: ".validateName($_POST['name'], 4, $_POST['color']).", długość: ".strlen(validateName($_POST['name'], 4, $_POST['color']))."</div>";
+        echo "Narodowość: ".validateNatio($_POST['nationality']);
+      }
+    ?>
+  </body>
+</html>
